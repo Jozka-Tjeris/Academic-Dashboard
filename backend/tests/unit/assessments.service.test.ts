401aaab1827095914b57317ef3539f3c45a31a85
Jozka Tjeris <jozkant@gmail.com>
2026-03-10 13:48:11 +1100

    tests: updated tests for updateAssessment, added tests for getAssessmentById and getAssessmentCollisions

--- backend/tests/unit/assessments.service.test.ts
+++ backend/tests/unit/assessments.service.test.ts
@@ -1,9 +1,14 @@
 import { Prisma } from "@prisma/client";
 import { prismaMock } from "../mocks/mockPrismaSingleton";
 import { getAssessmentServices } from "../../src/modules/assessments/assessment.service";
+import { AssessmentStatus } from "@internal_package/shared";
+import { HttpError } from "../../src/utils/httpError";
 
 describe("Assessment Service", () => {
   let service: ReturnType<typeof getAssessmentServices>;
+  type AssessmentWithCourse = Prisma.AssessmentGetPayload<{
+    include: { course: true };
+  }>;
 
   beforeEach(() => {
     service = getAssessmentServices(prismaMock);
@@ -259,6 +264,90 @@ describe("Assessment Service", () => {
         })
       ).rejects.toMatchObject({ status: 404 });
     });
+
+    const existingAssessment = {
+      assessmentId: "a1",
+      userId: "user1",
+      maxScore: new Prisma.Decimal(100),
+      courseId: "course1",
+      title: "",
+      description: null,
+      dueDate: new Date(),
+      score: null,
+      targetScore: null,
+      weight: new Prisma.Decimal(0),
+      latePenalty: null,
+      submitted: false,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+
+    it("updates allowed fields successfully", async () => {
+      prismaMock.assessment.findFirst.mockResolvedValue(existingAssessment);
+
+      const updated = { ...existingAssessment, title: "Updated Title" };
+
+      prismaMock.assessment.update.mockResolvedValue(updated);
+
+      const result = await service.updateAssessment({
+          userId: "user1",
+          assessmentId: "a1",
+          updates: {
+            title: "Updated Title",
+          }
+        });
+
+      expect(result.title).toBe("Updated Title")
+    });
+
+    it("does not mutate immutable fields", async () => {
+      prismaMock.assessment.findFirst.mockResolvedValue(existingAssessment);
+      prismaMock.assessment.update.mockResolvedValue(existingAssessment);
+
+      await expect(
+        service.updateAssessment({
+          userId: "user1",
+          assessmentId: "a1",
+          updates: {
+            assessmentId: "evil-change",
+          }
+        })
+      ).rejects.toMatchObject({ status: 400 });
+    });
+
+    it("sets submitted=true when score is provided", async () => {
+      prismaMock.assessment.findFirst.mockResolvedValue(existingAssessment);
+      prismaMock.assessment.update.mockResolvedValue({
+        ...existingAssessment,
+        score: new Prisma.Decimal(85),
+        submitted: true,
+      });
+
+      const result = await service.updateAssessment({
+        userId: "user1",
+        assessmentId: "a1",
+        updates: {
+          score: 80,
+        }
+      });
+
+      expect(result.submitted).toBe(true)
+    });
+
+    it("does not set submitted when score is null", async () => {
+      prismaMock.assessment.findFirst.mockResolvedValue(existingAssessment);
+      prismaMock.assessment.update.mockResolvedValue(existingAssessment);
+
+      const result = await service.updateAssessment({
+        userId: "user1",
+        assessmentId: "a1",
+        updates: {
+          score: null,
+        }
+      });
+
+      expect(result.submitted).toBe(false)
+    });
   });
 
   describe("deleteAssessment", () => {
@@ -302,4 +391,145 @@ describe("Assessment Service", () => {
       ).rejects.toMatchObject({ status: 404 });
     });
   });
+
+  describe("getAssessmentById", () => {
+    it("returns assessment when found", async () => {
+      const assessment = {
+        assessmentId: "a1",
+        courseId: "course1",
+        userId: "user1",
+        title: "",
+        description: null,
+        dueDate: new Date(),
+        score: new Prisma.Decimal(80),
+        targetScore: null,
+        weight: new Prisma.Decimal(0),
+        latePenalty: null,
+        maxScore: new Prisma.Decimal(100),
+        submitted: true,
+        createdAt: new Date("2026-03-10"),
+        updatedAt: new Date("2026-03-10"),
+      }
+      const assessmentWithCourse: AssessmentWithCourse = {
+        ...assessment,
+        course: {
+          courseId: "course1",
+          name: "courseName",
+          userId: "user1",
+          description: null,
+          createdAt: new Date("2026-03-10"),
+          updatedAt: new Date("2026-03-10"),
+        },
+      }
+
+      const resShape = {
+        assessment: {
+          ...assessment,
+        },
+        course: {
+          courseId: "course1",
+          name: "courseName",
+        },
+        derived: {
+          status: AssessmentStatus.GRADED,
+          urgencyScore: new Prisma.Decimal(0),
+        },
+      }
+
+      prismaMock.assessment.findUniqueOrThrow.mockResolvedValue(assessmentWithCourse);
+
+      const result = await service.getAssessmentById("a1", "user1");
+
+      expect(result).toEqual(resShape);
+      expect(prismaMock.assessment.findUniqueOrThrow).toHaveBeenCalled();
+    })
+
+    it("throws 404 when assessment does not exist", async () => {
+      prismaMock.assessment.findUniqueOrThrow.mockRejectedValue((
+        new HttpError(404, 'Assessment not found')
+      ));
+
+      await expect(service.getAssessmentById("missing", "user1"))
+        .rejects
+        .toThrow('Assessment not found');
+    })
+  })
+
+  describe("getAssessmentCollisions", () => {
+    it("returns empty array when no collisions", async () => {
+      const assessments = [{
+        assessmentId: "a1",
+        courseId: "course1",
+        userId: "user1",
+        title: "",
+        description: null,
+        dueDate: new Date(),
+        score: null,
+        targetScore: null,
+        weight: new Prisma.Decimal(0),
+        latePenalty: null,
+        maxScore: new Prisma.Decimal(100),
+        submitted: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }];
+
+      prismaMock.assessment.findMany.mockResolvedValue(assessments);
+
+      const result = await service.getAssessmentCollisions("user1");
+
+      expect(result).toMatchObject({ "clusters": []});
+    })
+
+    it("returns detected collisions", async () => {
+      const assessments = [{
+        assessmentId: "a1",
+        courseId: "course1",
+        userId: "user1",
+        title: "",
+        description: null,
+        dueDate: new Date("2026-03-10"),
+        score: null,
+        targetScore: null,
+        weight: new Prisma.Decimal(0),
+        latePenalty: null,
+        maxScore: new Prisma.Decimal(100),
+        submitted: false,
+        createdAt: new Date("2026-03-10"),
+        updatedAt: new Date("2026-03-10")
+      },
+      {
+        assessmentId: "a2",
+        courseId: "course1",
+        userId: "user1",
+        title: "",
+        description: null,
+        dueDate: new Date("2026-03-10"),
+        score: null,
+        targetScore: null,
+        weight: new Prisma.Decimal(0),
+        latePenalty: null,
+        maxScore: new Prisma.Decimal(100),
+        submitted: false,
+        createdAt: new Date("2026-03-10"),
+        updatedAt: new Date("2026-03-10")
+      }];
+
+      const collisions = {
+        "clusters": [
+          {
+            "assessmentIds": ["a1", "a2"], 
+            "count": 2, 
+            "endDate": "2026-03-10T00:00:00.000Z", 
+            "startDate": "2026-03-10T00:00:00.000Z"
+          }
+        ]
+      };
+      prismaMock.assessment.findMany.mockResolvedValue(assessments);
+
+      const result = await service.getAssessmentCollisions("user1");
+
+      expect(result).toEqual(collisions);
+    })
+  })
 });
