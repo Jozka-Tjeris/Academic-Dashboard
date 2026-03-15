945a1fa8ae592aabd17bb1c4bc90b54ae293e444
Jozka Tjeris <jozkant@gmail.com>
2026-03-16 01:14:45 +1100

    feat: added user avatar on top bar component

--- backend/src/modules/auth/auth.types.ts
+++ backend/src/modules/auth/auth.types.ts
@@ -2,6 +2,7 @@ export interface JwtPayload {
   sub: string;        // user.id (subject field)
   email: string;
   name?: string | null;
+  image?: string;
   iat?: number;
   exp?: number;
 }
--- frontend/src/components/layout/Topbar.tsx
+++ frontend/src/components/layout/Topbar.tsx
@@ -1,4 +1,5 @@
-import { useLogout } from "@/hooks/useAuth";
+import { useCheckAuth, useLogout } from "@/hooks/useAuth";
+import { UserAvatar } from "../user/UserAvatar";
 
 interface TopbarProps {
   title: string;
@@ -6,14 +7,23 @@ interface TopbarProps {
 
 export default function Topbar({ title }: TopbarProps) {
   const logout  = useLogout();
+  const { data } = useCheckAuth();
+
+  const user = { 
+    name: data && data.name !== null ? data.name : "", 
+    imageUrl: data ? data.image : null }; // Replace with real user data from context or hook
   
   return (
     <header className="h-16 bg-white border-b flex items-center justify-between px-6">
       <h1 className="text-xl font-semibold">{title}</h1>
 
-      <button className="w-9 h-9 rounded-full bg-gray-300" onClick={() => logout.mutate()}>
-        A
-      </button>
+      <div onClick={() => logout.mutate()}>
+        <UserAvatar
+          name={user.name}
+          imageUrl={user.imageUrl}
+          size={36}
+        />
+      </div>
     </header>
   );
 }
